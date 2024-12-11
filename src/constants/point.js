0576604aca9b1ec394f1b9c73c3183656ee09a0a
Keshav-Maheshwari <maheshwarikeshav029@gmail.com>
2024-12-11 23:15:38 +0530

    heatmap complete

---
+++ src/constants/point.js
@@ -0,0 +1,75 @@
+export const getPoints = () => {
+  // Replace random generation with actual data
+  return [
+    { location: new window.google.maps.LatLng(21.224231, 81.40835), aqi: 102 },
+    { location: new window.google.maps.LatLng(21.194815, 81.31477), aqi: 86 },
+    {
+      location: new window.google.maps.LatLng(28.6514781, 77.1473105),
+      aqi: 182,
+    },
+    { location: new window.google.maps.LatLng(28.710508, 77.249485), aqi: 207 },
+    { location: new window.google.maps.LatLng(28.56789, 77.250515), aqi: 278 },
+    { location: new window.google.maps.LatLng(28.73282, 77.170633), aqi: 251 },
+    {
+      location: new window.google.maps.LatLng(28.7500499, 77.1112615),
+      aqi: 175,
+    },
+    { location: new window.google.maps.LatLng(28.815329, 77.15301), aqi: 176 },
+    { location: new window.google.maps.LatLng(28.7762, 77.051074), aqi: 250 },
+    {
+      location: new window.google.maps.LatLng(28.6811736, 77.3025234),
+      aqi: 164,
+    },
+    { location: new window.google.maps.LatLng(28.628624, 77.24106), aqi: 177 },
+    { location: new window.google.maps.LatLng(28.588333, 77.221667), aqi: 148 },
+    { location: new window.google.maps.LatLng(28.684678, 77.076574), aqi: 286 },
+    { location: new window.google.maps.LatLng(28.623763, 77.287209), aqi: 205 },
+    { location: new window.google.maps.LatLng(28.639652, 77.146275), aqi: 234 },
+    { location: new window.google.maps.LatLng(28.674045, 77.131023), aqi: 219 },
+    { location: new window.google.maps.LatLng(28.63611, 77.173332), aqi: 137 },
+    {
+      location: new window.google.maps.LatLng(28.5918245, 77.2273074),
+      aqi: 152,
+    },
+    { location: new window.google.maps.LatLng(28.611281, 77.237738), aqi: 205 },
+    { location: new window.google.maps.LatLng(28.672342, 77.31526), aqi: 204 },
+    { location: new window.google.maps.LatLng(28.732528, 77.11992), aqi: 235 },
+    { location: new window.google.maps.LatLng(28.498571, 77.26484), aqi: 228 },
+    {
+      location: new window.google.maps.LatLng(28.5710274, 77.0719006),
+      aqi: 248,
+    },
+    {
+      location: new window.google.maps.LatLng(28.5627763, 77.1180053),
+      aqi: 230,
+    },
+    { location: new window.google.maps.LatLng(21.185571, 81.343175), aqi: 111 },
+    {
+      location: new window.google.maps.LatLng(28.4706914, 77.1099364),
+      aqi: 159,
+    },
+    { location: new window.google.maps.LatLng(28.531346, 77.190156), aqi: 220 },
+    { location: new window.google.maps.LatLng(28.656756, 77.227234), aqi: 190 },
+    { location: new window.google.maps.LatLng(28.822836, 77.101981), aqi: 206 },
+    { location: new window.google.maps.LatLng(28.636429, 77.201067), aqi: 225 },
+    { location: new window.google.maps.LatLng(28.60909, 77.0325413), aqi: 145 },
+    { location: new window.google.maps.LatLng(28.570173, 76.933762), aqi: 144 },
+    {
+      location: new window.google.maps.LatLng(28.5512005, 77.2735737),
+      aqi: 214,
+    },
+    {
+      location: new window.google.maps.LatLng(28.6573814, 77.1585447),
+      aqi: 261,
+    },
+    { location: new window.google.maps.LatLng(28.530785, 77.271255), aqi: 172 },
+    { location: new window.google.maps.LatLng(28.58028, 77.233829), aqi: 241 },
+    { location: new window.google.maps.LatLng(28.695381, 77.181665), aqi: 262 },
+    { location: new window.google.maps.LatLng(28.699793, 77.165453), aqi: 251 },
+    { location: new window.google.maps.LatLng(28.563262, 77.186937), aqi: 339 },
+    {
+      location: new window.google.maps.LatLng(28.5504249, 77.2159377),
+      aqi: 251,
+    },
+  ];
+};
